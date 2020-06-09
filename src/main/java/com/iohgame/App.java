fc1880cac15ec0b01ebe0eae9b717b6b97e78fd9
a <a>
2020-06-09 11:18:52 +0800

    a

--- src/main/java/com/iohgame/App.java
+++ src/main/java/com/iohgame/App.java
@@ -24,8 +24,8 @@ public class App extends MainClass
         VisualPanel panel = VisualPanel.getGridBagLayoutPanel();
         panel.setCons(0, 0, 1, 1, 1, 0).addTextLabel("枪械名");
         panel.setCons(1, 0, 1, 1, 1, 0).addTextLabel("等级");
-//        panel.setCons(2, 0, 1, 1, 1, 0).addTextLabel("击败");
-//        panel.setCons(3, 0, 1, 1, 1, 0).addTextLabel("伤害");
+        // panel.setCons(2, 0, 1, 1, 1, 0).addTextLabel("击败");
+        // panel.setCons(3, 0, 1, 1, 1, 0).addTextLabel("伤害");
         int i = 1;
         EnumSet<Weapon> enumList = EnumSet.allOf(Weapon.class);
         for (Weapon weapon : enumList)
@@ -36,7 +36,7 @@ public class App extends MainClass
             if (w != null)
             {
                 panel.setCons(0, i, 1, 1, 1, 0).addTextLabel(weapon.getName());
-                panel.setCons(1, i, 1, 1, 1, 0).addTextLabel("Lv."+w.level().toString());
+                panel.setCons(1, i, 1, 1, 1, 0).addTextLabel("Lv." + w.level().toString());
                 panel.setCons(2, i, 1, 1, 1, 0).addTextLabel(w.stats().defeats() + "");
                 panel.setCons(3, i, 1, 1, 1, 0).addTextLabel(w.stats().mostDefeatsInAGame() + "");
                 panel.setCons(4, i, 1, 1, 1, 0).addTextLabel(Math.floor(w.stats().damagePlayer()) + "");
@@ -48,19 +48,15 @@ public class App extends MainClass
                 panel.setCons(10, i, 1, 1, 1, 0).addTextLabel(w.stats().groggies() + "");
                 panel.setCons(11, i, 1, 1, 1, 0).addTextLabel(w.stats().mostGroggiesInAGame() + "");
             }
-            else {
+            else
+            {
                 panel.setCons(0, i, 1, 1, 1, 0).addTextLabel(weapon.getName());
                 panel.setCons(1, i, 1, 1, 1, 0).addTextLabel("Lv.0");
-                panel.setCons(2, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(3, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(4, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(5, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(6, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(7, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(8, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(9, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(10, i, 1, 1, 1, 0).addTextLabel("0");
-                panel.setCons(11, i, 1, 1, 1, 0).addTextLabel("0");
+                for (int cols = 2; cols < 12; cols++)
+                {
+                    panel.setCons(cols, i, 1, 1, 1, 0).addTextLabel("0");
+                }
+
             }
             i++;
         }
